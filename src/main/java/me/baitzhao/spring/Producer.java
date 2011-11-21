b1ff8efd5296272e99166d7c01fbd3e0535902ab
Bati Zhao <zhaobati@gmail.com>
2011-11-21 16:20:38 +0800

    Update Spring JMS Sample.

---
+++ src/main/java/me/baitzhao/spring/Producer.java
@@ -0,0 +1,20 @@
+package me.baitzhao.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+
+/**
+ * @author: batizhao
+ * @since: 11-11-21 下午2:41
+ */
+public class Producer {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new FileSystemXmlApplicationContext("classpath:spring.xml");
+        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+
+        jmsTemplate.convertAndSend("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
