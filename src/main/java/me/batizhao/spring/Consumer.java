4d7b1a7bf82c3f5167155ba9ca690af07a43791b
Bati Zhao <zhaobati@gmail.com>
2011-11-21 16:35:02 +0800

    Refactory Code.

--- src/main/java/me/batizhao/spring/Consumer.java
+++ src/main/java/me/batizhao/spring/Consumer.java
@@ -1,4 +1,4 @@
-package me.baitzhao.spring;
+package me.batizhao.spring;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
